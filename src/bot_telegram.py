e58d5c13f6f10a54cda5a1a69ee11f53708462ab
garrip91 <garrip91@gmail.com>
2022-04-15 14:08:43 +0300

    Update bot_telegram.py

--- src/bot_telegram.py
+++ src/bot_telegram.py
@@ -7,26 +7,34 @@ from config import TOKEN, ADMIN
 
 
 
+# await message.answer(message.text) # простой ответ бота
+# await message.reply(message.text) # ответ бота с цитированием
+# await bot.send_message(message.from_user.id, message.text) # ответ бота личным сообщением
+
+
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
+async def on_startup(_):
+    print('Бот вышел в онлайн')
 
 '''**************************************************************** КЛИЕНТСКАЯ ЧАСТЬ - НАЧАЛО ****************************************************************'''
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Приятного аппетита!')
+        await bot.send_message(message.from_user.id, 'Приятного аппетита!') # ответ бота личным сообщением
         await message.delete()
     except:
-        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/garrip91_bot')
+        await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/garrip91_bot') # ответ бота с цитированием
 
 @dp.message_handler(commands=['Режим_работы'])
 async def pizza_open_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Вс-Чт с 09:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 09:00 до 20:00, Пт-Сб с 10:00 до 23:00') # ответ бота личным сообщением
 
 @dp.message_handler(commands=['Расположение'])
 async def pizza_place_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'ул. Колбасная, 15')
+    await bot.send_message(message.from_user.id, 'ул. Колбасная, 15') # ответ бота личным сообщением
 '''**************************************************************** КЛИЕНТСКАЯ ЧАСТЬ - КОНЕЦ *****************************************************************'''
 
 '''***************************************************************** АДМИНСКАЯ ЧАСТЬ ****************************************************************'''
@@ -36,11 +44,7 @@ async def pizza_place_command(message: types.Message):
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    # await message.answer(message.text) # простой ответ бота
-    # await message.reply(message.text) # ответ бота с цитированием
-    # await bot.send_message(message.from_user.id, message.text) # ответ бота личным сообщением
-    if message.text == 'Привет':
-        await message.answer('И тебе привет!') # простой ответ бота
 
 
-executor.start_polling(dp, skip_updates=True)
+
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
